bbe240d9c6f092b5bcc31096e1c1ffd09eff3bdf
Ykgin <janvier20.yuyu.k@gmail.com>
2023-09-02 16:37:21 +0900

    修正２

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,14 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'は半角英数字で入力してください'
-  
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください'} do
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' } do
     validates :last_name
     validates :first_name
   end
-  with_options presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください'} do
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください' } do
     validates :last_name_kana, presence: true
     validates :first_name_kana, presence: true
   end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,17 +1,16 @@
 FactoryBot.define do
   factory :user do
     transient do
-      person {Gimei.name}
+      person { Gimei.name }
     end
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    last_name             {person.last.kanji}
-    first_name            {person.first.kanji}
-    last_name_kana        {person.last.katakana}
-    first_name_kana       {person.first.katakana}
-    birthday              {Faker::Date.birthday}
-
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    last_name             { person.last.kanji }
+    first_name            { person.first.kanji }
+    last_name_kana        { person.last.katakana }
+    first_name_kana       { person.first.katakana }
+    birthday              { Faker::Date.birthday }
   end
 end
