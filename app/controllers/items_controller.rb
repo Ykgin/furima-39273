623b466f4b26a6358e2acc1f522b5741db41de29
Ykgin <janvier20.yuyu.k@gmail.com>
2023-09-04 21:36:25 +0900

    item_categoryとitem_prefecture実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,26 @@
 class ItemsController < ApplicationController
   def index
+  
   end
 
   def new
-    
+    @item = Item.new
   end
 
+  def create
+    @Item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:items).permit(:item_name, :item_info, :item_category_id, :item_sales_status_id,
+                                  :item_shipping_fee_status_id, :item_prefecture_id, :item_schedule_delivery_id, 
+                                  :item_price, :image)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -11,11 +11,11 @@ class Item < ApplicationRecord
   
   validates :item_name, presence: true
   validates :item_info, presence: true
-  validates :item_category_id, numericality: { other_than: 1 }
-  validates :item_sales_status_id, numericality: { other_than: 1 }
-  validates :item_shipping_fee_status_id, numericality: { other_than: 1 }
-  validates :item_prefecture_id, numericality: { other_than: 1 }
-  validates :item_schedule_delivery_id, numericality: { other_than: 1 }
+  validates :item_category_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :item_sales_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :item_prefecture_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :item_schedule_delivery_id, numericality: { other_than: 1 , message: "can't be blank" }
   
   with_options presence: true, format: {with: /\A[0-9]+\z/, message: 'は半角で入力してください' } do
     validates :item_price
