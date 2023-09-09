3d7df1e395493482dae658abb9c2a54ce52d722e
Ykgin <janvier20.yuyu.k@gmail.com>
2023-09-09 15:16:37 +0900

    コード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -78,5 +78,10 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include( "Item price is not a number")
       end
+      it 'userが紐づいていなければ出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
     end
 end
