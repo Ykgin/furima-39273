0d02e82346121dd8e191a287ec7bcc0004e911e0
Ykgin <janvier20.yuyu.k@gmail.com>
2023-09-12 22:44:32 +0900

    情報更新時の条件分岐を設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,6 +26,15 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
